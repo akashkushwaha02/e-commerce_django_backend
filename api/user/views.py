14eff6749158b2c35544373469ef360964d1cb62
akash <akash.kushwaha02@gmail.com>
2021-02-27 10:13:38 +0530

    signin, signout and permission class added in user's view.py

--- api/user/views.py
+++ api/user/views.py
@@ -1,7 +1,20 @@
 from django.shortcuts import render
 import random
+from django.contrib.auth import login, logout
+from rest_framework import serializers
+from  rest_framework.permissions import AllowAny
+import re
+from django.http import JsonResponse
+from django.view.decorators.csrf import csrf_exempt
+from django.contrib.auth import get_user_model
+from .models import CustomUser
+from .serializers import UserSerializer
 # Create your views here.
 
 
 def generate_session_token(length=10):
     return ''.join(random.SystemRandom().choice([chr[i] for i in range(97,123)] + [str(i) for i in range(length)]) for _ in range(length))
+
+@csrf_exempt
+def signup(request):
+    
