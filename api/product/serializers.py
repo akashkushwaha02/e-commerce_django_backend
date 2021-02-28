b6ddb8c8555e655b63da465018790469812bd991
akash <akash.kushwaha02@gmail.com>
2021-02-28 13:19:40 +0530

    after debugging id bug in product's serializer

--- api/product/serializers.py
+++ api/product/serializers.py
@@ -6,4 +6,4 @@ class ProductSerializer(serializers.HyperlinkedModelSerializer):
     image  = serializers.ImageField(max_length=None,allow_empty_file=False,allow_null= True,required=False)
     class Meta:
         model = Product
-        fields = ['name','description','price','stock','is_active','image','category']
+        fields = ['id','name','description','price','stock','is_active','image','category']
