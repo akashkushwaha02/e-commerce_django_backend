b0bd35a19f1ed0c2984a9169925cdb30407e0fe0
akash <akash.kushwaha02@gmail.com>
2021-03-02 16:06:09 +0530

    Changes in views.py

--- api/payment/views.py
+++ api/payment/views.py
@@ -5,8 +5,6 @@ from django.contrib.auth import get_user_model
 from django.views.decorators.csrf import csrf_exempt
 import braintree
 
-# Create your views here.
-
 
 gateway = braintree.BraintreeGateway(
   braintree.Configuration(
@@ -17,43 +15,45 @@ gateway = braintree.BraintreeGateway(
   )
 )
 
-def validate_session_token(id,token):
-    userModel = get_user_model()
+def validate_user_session(id, token):
+    UserModel = get_user_model()
 
     try:
-        user = userModel.objects.get(pk=id)
+        user = UserModel.objects.get(pk=id)
         if user.session_token == token:
             return True
         return False
-    except userModel.DoesNotExist:
+    except UserModel.DoesNotExist:
         return False
 
-@csrf_exempt
-def generate_token(request,id,token):
-    if not validate_session_token(id,token):
-        return JsonResponse({'error':'Invalid session,Please login again'})
-
-    return JsonResponse({'clientToken':gateway.client_token.generate(),'success':'session geenrated succesfully'})
 
 @csrf_exempt
-def process_payment(request,id,token):
-    if not validate_session_token(id,token):
-        return JsonResponse({'error':'Invalid session,Please login again'})
+def generate_token(request, id, token):
+    if not validate_user_session(id, token):
+        return JsonResponse({'error': 'Invalid session, Please login again!'})
 
-    #this name come from frontend 
-    nonce_from_client = request.POST['paymentMethodNonce']
-    amount_from_client = request.POST['amount']
+    return JsonResponse({'clientToken': gateway.client_token.generate(), 'success': True})
+
+
+@csrf_exempt
+def process_payment(request, id, token):
+    if not validate_user_session(id, token):
+        return JsonResponse({'error': 'Invalid session, Please login again!'})
+
+    nonce_from_the_client = request.POST["paymentMethodNonce"]
+    amount_from_the_client = request.POST["amount"]
 
     result = gateway.transaction.sale({
-        "amount": amount_from_client,
-        "paymentMethodNonce": nonce_from_client,
+        "amount": amount_from_the_client,
+        "payment_method_nonce": nonce_from_the_client,
         "options": {
-            "submit_for_settlement":True 
+            "submit_for_settlement": True
         }
     })
 
     if result.is_success:
-        print(result)
-        return JsonResponse({'success':result.is_success,"transaction":{'id':result.transaction.id,"amount": result.transaction.amount}})
+        return JsonResponse({
+            # NOTE: transaction spelling bug was fixed later
+            "success": result.is_success, 'transaction': {'id': result.transaction.id, 'amount': result.transaction.amount}})
     else:
-        return JsonResponse({'error': True,'success':False})
+        return JsonResponse({'error': True, 'sucess': False})
